324718515b08384138a947f16394996d5322cd93
DIPAK PAUL <dipakpaul@DIPAK-PAULs-MacBook-Pro.local>
2020-03-02 21:42:26 +0530

    added conditional validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,53 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/user-name-validator';
+import { PasswordValidator } from './shared/password-validator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
+  registrationForm: FormGroup;
   get userName() {
     return this.registrationForm.get('userName');
   }
 
-  constructor(private fb: FormBuilder){}
-  registrationForm = this.fb.group({
-    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/admin/)]],
-    password: [''],
-    confirmPassword: [''],
-    address: this.fb.group({
-      street: ['Hosa Road'],
-      city: ['Bangalore'],
-      state: ['Karnataka'],
-      postalCode: ['560100']
-    })
-  })
+  get email() {
+    return this.registrationForm.get('email');
+  }
 
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(){
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/admin/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        street: ['Hosa Road'],
+        city: ['Bangalore'],
+        state: ['Karnataka'],
+        postalCode: ['560100']
+      })
+    }, {validator: PasswordValidator});
+
+    this.registrationForm.get('subscribe').valueChanges
+      .subscribe(checkedValue => {
+        const email = this.registrationForm.get('email');
+        if(checkedValue){
+          email.setValidators(Validators.required);
+        }else{
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      })
+  }
+  
   // registrationForm = new FormGroup({
   //   userName: new FormControl('Dipak'),
   //   password: new FormControl(''),
